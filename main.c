571c991ee11892b08c60415a5c8ce1d7d3454723
Paola Dorado Galicia <108164106+Paola-DG@users.noreply.github.com>
2024-12-06 23:06:46 -0500

    Update main - Betting Stages
    Where added some functions and mechanics: Dealer Button, Fold, Raise

--- main.c
+++ main.c
@@ -16,6 +16,8 @@ int playerTurn; //variable that determines if the user wants to fold(0), call(1)
 char anotherRound; //varable that determines if the user wants to play another round (Y or N).
 
 float userChips; //variable that stores the chips that the user has.
+float raiseAmount; //variable that stores the amount of money the bet has increased.
+float currentPot; //variable that stores the amount of money the pot has.
 int numRounds; //variable that stores the number of rounds that the user has played (before going to the main menu).
 int bigBlindBet = BLIND_BET * 2; //variable that stores the amount that the big blind bet cost.
 
@@ -29,7 +31,9 @@ void displayMainMenu(void); //function that displays the main menu of the progra
 void showRules(void); //function that displays the rules of the  Texas Hold’em - Poker Rules.
 void newGame(void); //function that displays the poker game.
 void showHeader(int newGame); //function that displays the title (and if newGame = 1, will display the blind bets and wallet).
+void anotherGame(void); //
 void playerActions(void); //function that 
+void dealerButton(void); //function that displays if you are the dealer, small blind, or big blind.
 
 void preflop(Table *table);
 
@@ -174,7 +178,8 @@ void showRules(void){
 void newGame(void){
     //local variables
     int minToGamble = bigBlindBet * 4; //variable that stores the minimum amout the user needs to be able to gamble.
-
+    currentPot = bigBlindBet;
+    
     srand(time(NULL)); //initializes the rand() function, giving different cards.
     numRounds++;
     showHeader(1); //showing title and blind bets
@@ -187,7 +192,7 @@ void newGame(void){
                 printf(" Press 1 to return to Main Menu: ");
                 scanf("%d", &userMainMenu);
             }
-    
+            
             if(userMainMenu == 1){
                 userMainMenu = 0;
                 system("clear");
@@ -204,6 +209,10 @@ void newGame(void){
             setup_dealer(&dealer, deck);
             setup_table(&table, &dealer, players);
         
+            //assign dealer button
+            system("clear");
+            showHeader(3);
+            
             //deal hole cards
             preflop(&table);
             
@@ -227,13 +236,14 @@ void newGame(void){
         initialize_deck(deck);
         shuffle_deck(deck);
         
-        //assign blinds - first round, user has the dealer button
+        //
         setup_players(players);
         setup_dealer(&dealer, deck);
         setup_table(&table, &dealer, players);
         
+        //assign dealer button
         system("clear");
-        showHeader(1);
+        showHeader(2);
         
         //deal hole cards
         preflop(&table);
@@ -241,7 +251,45 @@ void newGame(void){
         //pre-flop betting round
         playerActions();
         
+        //flop betting round
+        system("clear");
+        showHeader(4);
+        playerActions();
         
+        //turn betting round
+        system("clear");
+        showHeader(4);
+        playerActions();
+        
+        //final betting round
+        system("clear");
+        showHeader(4);
+        playerActions();
+        
+        //determine winner
+        system("clear");
+        showHeader(5);
+        
+        anotherGame();
+    }
+}
+
+void anotherGame(void){
+    printf(" End of the game. Wallet: %.2f \n", userChips);
+    printf(" Do you want to play another round? (Y/N): ");
+    scanf("%s", &anotherRound);
+            
+    while(anotherRound != 'Y' && anotherRound != 'y' && anotherRound != 'N' && anotherRound != 'n'){
+        printf(" Invalid Input. Type again your response \n");
+        printf(" Do you want to play another round? (Y/N): ");
+        scanf("%s", &anotherRound);
+    }
+    if(anotherRound == 'Y' || anotherRound == 'y'){
+        system("clear");
+        newGame(); //Go back to newGame window.
+    }else{
+        system("clear");
+        main(); //Go back to Main window.
     }
 }
 
@@ -254,48 +302,79 @@ void preflop(Table *table){
 }
 
 void playerActions(void){
-    printf("\n");
-    printf("---------------------------\n");
-    printf(" Fold  || press 0 \n");
-    printf("---------------------------\n");
-    printf("---------------------------\n");
-    printf(" Call  || press 1 \n");
-    printf("---------------------------\n");
-    printf("---------------------------\n");
-    printf(" Raise || press 2 \n");
-    printf("---------------------------\n");
-    printf("\n");
-    
-    printf(" Player Choice: ");//to-do: delete after input
-    scanf("%d", &playerTurn);
-    
-    if(playerTurn != 0 && playerTurn != 1 && playerTurn != 2){
-        printf(" Invalid Input. Type again your response \n");
-        printf(" Player Choice: "); //to-do: delete after input
-        scanf("%d", &playerTurn);
-    }else if(playerTurn == 0){
-        printf(" End of the game. Wallet: %.2f \n", userChips);
-        printf(" Do you want to play another round? (Y/N): ");
-        scanf("%s", &anotherRound);
-        
-        while(anotherRound != 'Y' && anotherRound != 'y' && anotherRound != 'N' && anotherRound != 'n'){
-            printf(" Invalid Input. Type again your response \n");
-            printf(" Do you want to play another round? (Y/N): ");
-            scanf("%s", &anotherRound);
-        }
-        
-        if(anotherRound == 'Y' || anotherRound == 'y'){
-            system("clear");
-            newGame(); //Go back to newGame window.
-        }else{
-            system("clear");
-            main(); //Go back to Main window.
-        }
-        
-    }else if(playerTurn != 1){
-        printf(" CALL \n");
+    if(userChips <= 0){
+        printf(" You do not have enough money to keep playing. Go back to main menu to start a new game! \n");
+           while(userMainMenu != 1){
+                printf(" Press 1 to return to Main Menu: ");
+                scanf("%d", &userMainMenu);
+            }
+            
+            if(userMainMenu == 1){
+                userMainMenu = 0;
+                system("clear");
+                main(); //Go back to main function.
+            }
     }else{
-        printf(" RAISE \n");
+        printf("\n");
+        printf("---------------------------\n");
+        printf(" Fold  || press 0 \n");
+        printf("---------------------------\n");
+        printf("---------------------------\n");
+        printf(" Call  || press 1 \n");
+        printf("---------------------------\n");
+        printf("---------------------------\n");
+        printf(" Raise || press 2 \n");
+        printf("---------------------------\n");
+        printf("\n");
+        
+        printf(" Player Choice: ");//to-do: delete after input
+        scanf("%d", &playerTurn);
+        
+        while(playerTurn != 0 && playerTurn != 1 && playerTurn != 2){
+            printf(" Invalid Input. Type again your response \n");
+            printf(" Player Choice: "); //to-do: delete after input
+            scanf("%d", &playerTurn);
+        }
+        if(playerTurn == 0){
+           anotherGame();
+           
+        }else if(playerTurn == 1){
+            printf(" CALL \n");
+            
+        }else{
+            printf(" Enter the amount of money you want to raise the bet: $");
+            scanf("%f", &raiseAmount);
+            
+            int x = 0;
+            while( x != 1){
+                if(raiseAmount <= currentPot){
+                    printf(" To raise, the amount has to be greater that $%.2f \n", currentPot);
+                    printf(" Type again your bet: $");
+                    scanf("%f", &raiseAmount);
+                }else if(raiseAmount > userChips){
+                    printf(" You do not have enough money to bet. Wallet: $%.2f \n", userChips);
+                    printf(" Type again your bet: $");
+                    scanf("%f", &raiseAmount);
+                }else{
+                    userChips -= raiseAmount;
+                    currentPot += raiseAmount;
+                    x = 1;
+                }
+            }
+            
+        }
+    }
+}
+
+void dealerButton(void){
+    if(numRounds %3 == 2){
+        printf(" You are the Big Blind \n");
+        userChips -= bigBlindBet;
+    }else if(numRounds % 3 == 0){
+         printf(" You are the Small Blind \n");
+         userChips -= BLIND_BET;
+    }else{
+        printf(" You have the Dealer Button \n");
     }
 }
 
@@ -307,14 +386,71 @@ void showHeader(int newGame){
     printf("                                     Texas Hold’em - Poker Game \n");
     printf("------------------------------------------------------------------------------------------\n");
     
-    if(newGame == 1){
+    switch (newGame){
+        case 1:
         printf("--------------------------------------------------------------------\n");
         printf(" Blind Bet: $%d | Big Blind Bet: $%d | Minimum amount to play: $%d  \n", BLIND_BET, bigBlindBet, minToGamble);
         printf("--------------------------------------------------------------------\n");
     
         printf("--------------------------------------------------------------------\n");
         printf(" Welcome to Round %d | Wallet: $%.2f \n", numRounds, userChips);
-        printf("--------------------------------------------------------------------\n");  
+        printf("--------------------------------------------------------------------\n");
+        break;
+        
+        case 2:
+        printf("--------------------------------------------------------------------\n");
+        dealerButton();
+        printf("--------------------------------------------------------------------\n");
+        
+        printf("--------------------------------------------------------------------\n");
+        printf(" Wallet: $%.2f \n", userChips);
+        printf("--------------------------------------------------------------------\n");
+        break;
+        
+        case 3:
+        printf("--------------------------------------------------------------------\n");
+        dealerButton();
+        printf("--------------------------------------------------------------------\n");
+        
+        printf("--------------------------------------------------------------------\n");
+        printf(" Welcome to Round %d | Wallet: $%.2f \n", numRounds, userChips);
+        printf("--------------------------------------------------------------------\n");
+        break;
+        
+        case 4:
+        printf("--------------------------------------------------------------------\n");
+        printf(" Pot: $%.2f | Wallet: $%.2f \n", currentPot, userChips);
+        printf("--------------------------------------------------------------------\n");
+        
+        printf("--------------------------------------------------------------------\n");
+        printf(" Your Hole Cards: \n");
+        printf("--------------------------------------------------------------------\n");
+        
+        printf("--------------------------------------------------------------------\n");
+        printf(" Table Hole Cards: \n");
+        printf("--------------------------------------------------------------------\n");
+        break;
+        
+        case 5:
+        printf("--------------------------------------------------------------------\n");
+        printf(" Table Hole Cards: \n");
+        printf("--------------------------------------------------------------------\n");
+        
+        printf("--------------------------------------------------------------------\n");
+        printf(" Your Poker Hand:       X   |   Your Cards:     X\n");
+        printf(" Bobby's Poker Hand:    X   |   Bobby's Cards:  X\n");
+        printf(" Robby's Poker Hand:    X   |   Robby's Cards:  X\n");
+        printf("--------------------------------------------------------------------\n");
+        
+        printf("--------------------------------------------------------------------\n");
+        printf(" Winner X, earns $%.2f \n", currentPot);
+        printf("--------------------------------------------------------------------\n");
+        
+        
+        
+        
+        break;
+        
     }
 
 }
